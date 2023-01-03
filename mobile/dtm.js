8b1434ed70c4196ad255f8481f8295343925648f
Seb <69688534+SebTNT@users.noreply.github.com>
2023-01-03 18:21:59 +0000

    Update dtm.js

--- mobile/dtm.js
+++ mobile/dtm.js
@@ -1,7 +1,7 @@
 // DTM - DESKTOP TO MOBILE
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     // true for mobile device
-    window.location.replace("https://turquoisetnt.tk/mobile.html");
+    window.location.replace("https://tnt.is-a.dev/mobile.html");
   }else{
     // false for not mobile device
     console.log("[SITE CHANGER - DTM]: Site is Correct, DESKTOP.");
